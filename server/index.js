6530dc3d119878af9f6164ab245af5b1b945264f
Neel100606 <neelmendapara@gmail.com>
2025-12-27 11:10:20 +0530

    Basic App initialisation...

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import "./firebaseAdmin.js"   // initializes Firebase Admin
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("AccessPath Backend Running 🚀")
+})
+
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
